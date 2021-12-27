33542cfbccc4cfbcbea183f0297e34b3735a96da
marshall-hobbs96 <marshall.hobbs96@gmail.com>
2021-12-27 16:16:33 -0600

    initialized maven project

---
+++ src/main/java/com/revature/LostChapter/LostChapterE2EApplication.java
@@ -0,0 +1,13 @@
+package com.revature.LostChapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostChapterE2EApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostChapterE2EApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/LostChapter/LostChapterE2EApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.LostChapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostChapterE2EApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
