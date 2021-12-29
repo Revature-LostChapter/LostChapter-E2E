a3b5b7a9a42df21223c81631e274a9716c8aa6c6
marshall-hobbs96 <marshall.hobbs96@gmail.com>
2021-12-29 09:54:20 -0600

    Created several page object models

---
+++ src/test/java/com/revature/models/pages/LoginPage.java
@@ -0,0 +1,21 @@
+package com.revature.models.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.revature.models.components.LoginComponent;
+import com.revature.models.components.NavbarComponent;
+
+public class LoginPage {
+	
+	public NavbarComponent navbar;
+	public LoginComponent login; 
+
+	
+	public LoginPage(WebDriver driver) {
+		
+		this.navbar = new NavbarComponent(driver);
+		this.login = new LoginComponent(driver);
+		
+	}
+	
+}
