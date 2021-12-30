6fc64b3b757ce3ed5a603bbd624628af8f8d7fbb
Bach Tran <bach.tran@revature.com>
2021-12-30 15:52:48 -0600

    Modern configuration for TestRunner

--- src/test/java/com/revature/features/TestRunner.java
+++ src/test/java/com/revature/features/TestRunner.java
@@ -1,8 +1,14 @@
 package com.revature.features;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@Cucumber
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("com/revature/features")
+@ConfigurationParameter(key = "cucumber.glue", value = "com.revature.tests")
 public class TestRunner {
 
 }
