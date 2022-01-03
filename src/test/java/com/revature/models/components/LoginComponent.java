4a4a485024540badc815be3727c5101c08117be8
Mike Dingeldein <mike.dingeldein@revature.net>
2022-01-03 13:31:28 -0700

    added to LoginTest

--- src/test/java/com/revature/models/components/LoginComponent.java
+++ src/test/java/com/revature/models/components/LoginComponent.java
@@ -10,82 +10,84 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginComponent {
-	
+
 	private WebDriver driver;
-	private WebDriverWait wdw; 
-	
+	private WebDriverWait wdw;
+
 	@FindBy(xpath = "//input[@id='username']")
 	private WebElement usernameField;
-	
+
 	@FindBy(xpath = "//input[@id='password']")
-	private WebElement passwordField; 
-	
+	private WebElement passwordField;
+
 	@FindBy(xpath = "//button[contains(text(),'Log in')]")
 	private WebElement loginButton;
-	
+
 	@FindBy(xpath = "")
 	private WebElement forgotPasswordLink;
-	
+
 	@FindBy(xpath = "//a[contains(text(),\"Don't have an account? Create a new account\")]")
-	private WebElement createAccountButton; 
-	
-	//Missing error message
-	
+	private WebElement createAccountButton;
+
+	// Missing error message
+	@FindBy(xpath = "")
+	private WebElement errorMessage;
+
 	public LoginComponent(WebDriver driver) {
-		
-		this.driver = driver; 
+
+		this.driver = driver;
 		wdw = new WebDriverWait(this.driver, 2);
-		
+
 		PageFactory.initElements(this.driver, this);
 
-		
-		
-		
 	}
-	
-	
+
 	public String getUsernameText() {
-		
+
 		return wdw.until(ExpectedConditions.visibilityOf(usernameField)).getText();
-		
+
 	}
-	
+
 	public void setUsernameText(String username) {
-		
+
 		wdw.until(ExpectedConditions.visibilityOf(usernameField)).sendKeys(username);
-		
+
 	}
-	
+
 	public String getPasswordText() {
-		
+
 		return wdw.until(ExpectedConditions.visibilityOf(passwordField)).getText();
-		
-		
+
 	}
-	
+
 	public void setPasswordText(String password) {
-		
+
 		wdw.until(ExpectedConditions.visibilityOf(passwordField)).sendKeys(password);
-		
+
 	}
-	
+
 	public void clickLoginButton() {
-		
+
 		wdw.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
-		
+
 	}
-	
+
 	public void clickForgotPasswordButton() {
-		
+
 		wdw.until(ExpectedConditions.elementToBeClickable(forgotPasswordLink)).click();
-		
+
 	}
-	
+
 	public void clickCreateAccountButton() {
-		
+
 		wdw.until(ExpectedConditions.elementToBeClickable(createAccountButton)).click();
-		
+
 	}
-	
+
+	public String getErrorMessage() {
+
+		return wdw.until(ExpectedConditions.visibilityOf(errorMessage)).getText();
+
+	}
 
 }
